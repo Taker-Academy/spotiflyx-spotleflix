a01d39d5bdd915024350ba0f50b2d9ba67234b4a
yanisdolivet <yanis.dolivet@epitech.eu>
2024-04-25 13:08:08 +0200

    [add]: initial commit

---
+++ front/spotiflyx/src/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import cn from "classnames";
+import  localFont  from 'next/font/local';
+
+const YoutubeFont = localFont({ src: '../components/fonts/Youtube-Sans-font/YoutubeSansBold.woff2' })
+const SpotifyFont = localFont({ src: '../components/fonts/Spotify-Font/GothamBold.woff2' })
+
+export const metadata: Metadata = {
+  title: "Spotiflyx",
+  description: "The new streaming app revolution",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={cn(
+        YoutubeFont.className,
+        SpotifyFont.className
+        )}
+        >
+          {children}
+        </body>
+    </html>
+  );
+}
