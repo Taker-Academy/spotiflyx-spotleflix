5c822ad701e2ae4a59651ba25093fee8372f13e3
Yanis Mignot <yanis.mignot@epitech.eu>
2024-05-02 18:07:18 +0200

    [ADD] search popular video in youtube

--- backend/youtube/routes/routes_search.js
+++ backend/youtube/routes/routes_search.js
@@ -1,7 +1,10 @@
 const express = require("express");
-const { setGetSearch } = require("../controls/search/control_getSearch");
 const router = express.Router();
+const { setGetSearch } = require("../controls/search/control_getSearch");
+const { setGetPop } = require("../controls/search/control_getPop");
 
 router.get("/search", setGetSearch);
+
+router.get("/popular", setGetPop);
 
 module.exports = router;
